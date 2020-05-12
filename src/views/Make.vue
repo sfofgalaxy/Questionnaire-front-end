4280ae249b980019694456aca7dbd2277060eed0
sfofgalaxy <3170105860@zju.edu.cn>
2020-05-12 17:19:09 +0800

    version 1.0

--- src/views/Make.vue
+++ src/views/Make.vue
@@ -1,5 +1,18 @@
 <template>
-<Logo/>
+  <el-container>
+    <el-aside width="200px">
+      <Navigator/>
+    </el-aside>
+    <el-container>
+      <el-header style="font-size: 18px;color: #303133;">
+        问卷调查网站
+      </el-header>
+      <el-main>
+        制作并发布问卷
+      </el-main>
+      <el-footer>Design ©2020 by 彭子帆 3170105860@zju.edu.cn</el-footer>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
--- src/views/MyPaper.vue
+++ src/views/MyPaper.vue
@@ -1,9 +1,28 @@
 <template>
-
+  <el-container>
+    <el-aside width="200px">
+      <Navigator/>
+    </el-aside>
+    <el-container>
+      <el-header style="font-size: 18px;color: #303133;">
+        问卷调查网站
+      </el-header>
+      <el-main>
+        我的问卷网站
+      </el-main>
+      <el-footer>Design ©2020 by 彭子帆 3170105860@zju.edu.cn</el-footer>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+    import Navigator from '@/components/Navigator';
+    import Logo from '@/components/Logo';
     export default {
+        components:{
+            Navigator,
+            Logo
+        },
         name: "MyPaper.vue"
     }
 </script>
